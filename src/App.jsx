19353134259f900827f4e8414fe0f528282bc560
Rajesh-Rampersad <danigarcia.negocio@gmail.com>
2024-01-31 17:09:40 -0300

    nro198

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { Productos } from './components/Productos';
 import { Layout } from './components/layout';
 import { Title } from './components/title';
@@ -7,24 +8,29 @@ import './App.css'
 import { Carro } from './components/Carro';
 
 export function App() {
-  const state = {
+  const [state, setState] = useState({
     productos: [
       { name: 'Tomate', price: 1500, img: '/productos/tomate.jpg' },
       { name: 'Arbeja', price: 2500, img: '/productos/arbejas.jpg' },
       { name: 'Lechuga', price: 500, img: '/productos/lechuga.jpg' },
     ],
     carro: [],
-  }
+  });
 
-
-
+  const agregarAlCarrito = (producto) => {
+    setState((prevState) => ({
+      ...prevState,
+      carro: [...prevState.carro, { ...producto, cantidad: 1 }],
+    }));
+    console.log(state.carro);
+  };
   return (
 
     < div >
       <Navbar />
       <Layout>
         <Title />
-        <Productos agregarAlCarrito={() => console.log('vacio')}
+        <Productos agregarAlCarrito={agregarAlCarrito}
           productos={state.productos} />
 
       </Layout>
